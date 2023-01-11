88bb92d9a39ccd52c667f31a905b5034e7671e6c
Thiagohrcosta <thiagohrcosta86@gmail.com>
2023-01-10 21:04:49 -0300

    adding requests

--- src/server.js
+++ src/server.js
@@ -1,17 +1,26 @@
 import http from 'http';
 
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários')
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
-    return res.end('Criação de usuário')
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@email.com'
+    })
+    return res.writeHead(201).end();
   }
 
-  return res.end("Hello World")
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
