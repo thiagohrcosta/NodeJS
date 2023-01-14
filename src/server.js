6b52826519d966a4840065390c7e795d1cdc9e88
Thiagohrcosta <thiagohrcosta86@gmail.com>
2023-01-14 15:18:00 -0300

    adding middlewares

--- src/server.js
+++ src/server.js
@@ -1,25 +1,15 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
 const users = []
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
+  await json(req, res)
 
   if (method === 'GET' && url === '/users') {
     return res
-      .setHeader('Content-type', 'application/json')
       .end(JSON.stringify(users))
   }
 
@@ -38,4 +28,4 @@ const server = http.createServer(async (req, res) => {
   return res.writeHead(404).end()
 })
 
-server.listen(3333) 
+server.listen(3333)
