c02019448486300c39165b67bd2fa3a104a03533
Thiagohrcosta <thiagohrcosta86@gmail.com>
2023-01-17 01:27:30 -0300

    Creating routes

--- src/server.js
+++ src/server.js
@@ -1,28 +1,19 @@
 import http from 'node:http'
-import { json } from './middlewares/json.js'
 
-const users = []
+import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
-  if (method === 'GET' && url === '/users') {
-    return res
-      .end(JSON.stringify(users))
-  }
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
 
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
-
-    users.push({
-      id: 1,
-      name,
-      email,
-    })
-
-    return res.writeHead(201).end()
+  if (route) {
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end()
